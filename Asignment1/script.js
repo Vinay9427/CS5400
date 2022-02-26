e8546feb9d968ffe8e5d15e3232e349a249716cf
Vinay9427 <dupativinay.94@gmail.com>
2022-02-25 19:48:54 -0600

    remaining with writing file'

--- Asignment1/script.js
+++ Asignment1/script.js
@@ -1,13 +1,11 @@
 const fs = require('fs')
 
 let a;
-let isInitialPenguPosition = false;
-let fishArray = [];
-let penguPosition ;
-let hazardArray = [];
+let isInitialPenguPosition = true;
+let penguInitialPosition ;
 let pathArray = [];
 let counter = 0;
-let fishCount = 0;
+
 let directions = [
                   [1, [1, -1]],
                   [2, [1, 0]],
@@ -30,91 +28,83 @@ function readAndLoadPositions(){
       return
     }
     a = data.split('\n').map(i => i.trim().split('')).slice(1)
-    log(a)
+    //log(a)
     for(let i = 0; i< a.length;i++){
       for(let j = 0; j< a[0].length;j++){
-        // if(a[i][j] == 'U' || a[i][j] == 'S'){
-        //   hazardArray.push([i,j]);
-        // }
         if(a[i][j] == 'P'){
-          penguPosition = [i,j]
+          penguInitialPosition = [i,j]
         }
-        // if(a[i][j] == '*'){
-        //   fishArray.push([i,j])
-        // }
       }
     }
-    penguSlider(a, penguPosition, counter)
+    a[penguInitialPosition[0]][penguInitialPosition[1]] = ' ';
+    let output = [];
+    
+    pathArray = [];
+      isInitialPenguPosition = true;
+      const tempGrid = JSON.parse(JSON.stringify(a));
+      output = penguSlider(tempGrid, penguInitialPosition, counter)
+    do{
+      pathArray = [];
+      isInitialPenguPosition = true;
+      fishCount = 0;
+      const tempGrid = JSON.parse(JSON.stringify(a));
+      output = penguSlider(tempGrid, penguInitialPosition, counter, fishCount)
+      console.log(output,'---------------------------start again');
+    }while(output[1].length !== 6)
   })  
 
 }
 
-function penguSlider(grid, penguPosition, counter){
-  //grid[penguPosition[0]][penguPosition[1]] = ' ';
-  let continueSliding = false;
+function penguSlider(grid, penguPosition, counter, fishCount){
   
   if(grid[penguPosition[0]][penguPosition[1]] == 'U' || grid[penguPosition[0]][penguPosition[1]] == 'S'){
     console.log('----------')
-    console.log('Caught by Bear/ Shark!'); 
-    console.log('pathArray: ',pathArray)
-    console.log('fishCount: ', fishCount)
-    console.log('----------')
+    console.log('Caught by Bear/ Shark!')
+    grid[penguInitialPosition[0]][penguInitialPosition[1]] = ' ';
     grid[penguPosition[0]][penguPosition[1]] = 'X';
-    return;
+    // log(grid, pathArray, fishCount)
+    console.log('----------')
+    return [grid, pathArray, fishCount];
   }
-
+  console.log(penguPosition);
+  if((isInitialPenguPosition || grid[penguPosition[0]][penguPosition[1]] == '0') && counter < 6){
+    moves = getNextMoves(penguPosition);
+    isInitialPenguPosition = false;
+    const [direction, randomMove] = getRandomValidMove(moves, grid);
+    pathArray.push(direction)
+    counter++;
+    return penguSlider(grid, randomMove, counter, fishCount);
+  }
+  // after eating fish the cell will be the ice cell so it does not need return statement
+  if(grid[penguPosition[0]][penguPosition[1]] == '*'){
+    log('Fish got caught!', penguPosition); 
+    fishCount++;
+    grid[penguPosition[0]][penguPosition[1]] = ' ';
+    //log('Fish got caught!', penguPosition,'direction',direction,'nextMove', nextMove, randomMove);
+  }
+  if(grid[penguPosition[0]][penguPosition[1]] == ' '){
+    //const nextMove = continueInTheSameDirection(direction, randomMove)
+    const direction = pathArray[pathArray.length-1];
+    const nextMove = continueInTheSameDirection(direction, penguPosition)
+    if(grid[nextMove[0]][nextMove[1]] == '#' && counter < 6){
+      moves = getNextMoves(penguPosition);
+      const [direction1, randomMove1] = getRandomValidMove(moves, grid);  
+      pathArray.push(direction1)
+      counter++;
+      return penguSlider(grid, randomMove1, counter, fishCount);
+    }else if(grid[nextMove[0]][nextMove[1]] != '#'){
+      console.log('asdfasdf')
+      return penguSlider(grid, nextMove, counter, fishCount)
+    }
+  }
   if(counter == 6){
     console.log('----------')
     console.log('Counter 6')
-    console.log('pathArray: ',pathArray)
-    console.log('fishCount: ', fishCount)
+    grid[penguPosition[0]][penguPosition[1]] = 'P';
+    log(grid, 'pathArray: ', pathArray, 'fishCount: ',fishCount)
     console.log('----------')
-    return;
+    return [grid, pathArray, fishCount];
   }
-
-  if(!isInitialPenguPosition){
-
-  }
-
-  const moves = getNextMoves(penguPosition);
-  const randomMove = getRandomValidMove(moves, grid);
-  log('P',penguPosition, 'NextMove',randomMove,'counter',counter)
-
-  if(grid[penguPosition[0]][penguPosition[1]] == '*'){
-    log('Fish got caught!', penguPosition); 
-    grid[penguPosition[0]][penguPosition[1]] = ' ';
-    fishCount++;
-
-    const nextMove = continueInTheSameDirection(randomMove[0], randomMove[1])
-    log('Fish got caught!', penguPosition,'nextMove', nextMove, randomMove); 
-    if(grid[nextMove[0]][nextMove[1]] == '#'){
-      penguSlider(grid, continueInTheSameDirection(randomMove[0], randomMove[1]))
-    }else{
-
-    }
-  }
-
-  if(grid[penguPosition[0]][penguPosition[1]] == '0'){
-    pathArray.push(randomMove[0])
-    counter++;
-    penguPosition = randomMove[1];
-    //grid[penguPosition[0]][penguPosition[1]] = 'P';      
-    penguSlider(grid, penguPosition, counter)
-  }
-
-  if(grid[penguPosition[0]][penguPosition[1]] == '#'){
-    
-    pathArray.push(randomMove[0])
-    penguPosition = randomMove[1];
-    //grid[penguPosition[0]][penguPosition[1]] = 'P';
-    counter++;
-    penguSlider(grid, penguPosition, counter)
-  }
-
-  penguSlider(grid, randomMove[1], counter)
-  // while(counter < 6){
-
-  // }
 }
 
 // Perform this when Pengu gets stuck by hitting a Wall or by the Snow cell
@@ -123,13 +113,13 @@ function getNextMoves(penguPosition){
   const x = penguPosition[0];
   const y = penguPosition[1];
   directions.forEach(item => moves.push([item[0],[x+item[1][0], y+item[1][1]]]))
-  //console.log(moves)
   return moves;
 }
 
 function getRandomValidMove(moves, grid){
   const randMoves = [];
-  moves.forEach((item, index) => {
+  moves.forEach((item) => {
+    //log('getRandomValidMove',item)
     if(item != undefined){
       if(grid[item[1][0]][item[1][1]] != '#'){
         randMoves.push([item[0], item[1]])
@@ -140,22 +130,11 @@ function getRandomValidMove(moves, grid){
   return [randMoves[randomIndex][0], randMoves[randomIndex][1]]
 }
 
-function continueInTheSameDirection(path, currentPos, multiplier){
-  const coordinateForTheDirection = directions.find(dir => dir[0] == path)[1];
+function continueInTheSameDirection(direction, currentPos, multiplier){
+  const coordinateForTheDirection = directions.find(dir => dir[0] == direction)[1];
   const [nextX, nextY] = coordinateForTheDirection;
-  // const pathInTheDirection = [];
-  // let i = 0;
-  // while(i < multiplier){
-  //   i = i + 1;
-  //   pathInTheDirection.push([multiplier*coordinateForTheDirection[0]+currentPos[0], multiplier*coordinateForTheDirection[1]+currentPos[1]])
-  // }
-  // return pathInTheDirection;
   const nextMove = [currentPos[0]+nextX, currentPos[1]+nextY]
-  // if(a[nextMove[0]][nextMove[1]] == '#'){
-  //   return;
-  // }
-  console.log(nextMove)
-  //return nextMove;
+  return nextMove;
 }
 
 
