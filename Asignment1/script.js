60951e937e64b08bc4ea2e5c236e623c34121878
Vinay9427 <dupativinay.94@gmail.com>
2022-02-27 21:17:01 -0600

    adding coments to the script file

--- Asignment1/script.js
+++ Asignment1/script.js
@@ -1,12 +1,15 @@
+// importing fs module for file operations
 const fs = require('fs')
 
-let a;
-let isInitialPenguPosition = true;
-let penguInitialPosition ;
-let pathArray = [];
-let counter = 0;
-let fishCount = 0;
-let initailFishcount = 0;
+let initialGrid; // Declaring the Grid
+let isInitialPenguPosition = true; // this helps us to trigger the penguSlider 
+let penguInitialPosition ; 
+let pathArray = []; // This array consists of the path travelled by the Pengu
+let counter = 0; // counter to check the count of the path
+let fishCount = 0; // counter to count the fishes caught by the Pengu
+let totalFishcount = 0;
+
+// Below directions are helpful for finding the co-ordinates wrt the direction of the Pengu
 let directions = [
                   [1, [1, -1]],
                   [2, [1, 0]],
@@ -22,50 +25,55 @@ function log(...params){
   return console.log(params)
 }
 
+// Reading the arguments(input and output file names) from the bash file and initializing to the variables
 const [inputFile, outputFile] = process.argv.slice(2);
 
-log(inputFile, outputFile)
-
+// this function reads the input file and note the pengu position and grid to 2d array
 function readAndLoadPositions(){
+  // Reading the input file which is captured in inputFile variabl
   fs.readFile(inputFile, 'utf8' , (err, data) => {
     if (err) {
       console.error(err)
       return
     }
-    a = data.split('\n').map(i => i.trim().split('')).slice(1)
-    //log(a)
-    for(let i = 0; i< a.length;i++){
-      for(let j = 0; j< a[0].length;j++){
-        if(a[i][j] == 'P'){
-          penguInitialPosition = [i,j]
+    // initializing the grid data into initialGrid
+    initialGrid = data.split('\n').map(i => i.trim().split('')).slice(1)
+    for(let i = 0; i< initialGrid.length;i++){
+      for(let j = 0; j< initialGrid[0].length;j++){
+        // finding Pengus position
+        if(initialGrid[i][j] == 'P'){
+          // initializing penguInitialPosition
+          penguInitialPosition = [i,j];
         }
-        if(a[i][j] == '*'){
-          initailFishcount++
+        // finding total no of Fishes
+        if(initialGrid[i][j] == '*'){
+          totalFishcount++;
         }
       }
     }
-    console.log(initailFishcount)
-    a[penguInitialPosition[0]][penguInitialPosition[1]] = ' ';
-    let output = penguSlider(a, penguInitialPosition, counter)
-    //console.log(output)
-    writeOutputToFile(output)
+    // Clearing the initial pengu position
+    initialGrid[penguInitialPosition[0]][penguInitialPosition[1]] = ' ';
+    // Calling penguSLider function
+    let output = penguSlider(initialGrid, penguInitialPosition, counter)
+    writeOutputToFile(output) // Writing output to the file
   })  
-
 }
 
 function penguSlider(grid, penguPosition, counter){
-  
+  // If Pengu caught by a Hazard
   if(grid[penguPosition[0]][penguPosition[1]] == 'U' || grid[penguPosition[0]][penguPosition[1]] == 'S'){
     console.log('----------')
     console.log('Caught by Bear/ Shark!')
     grid[penguInitialPosition[0]][penguInitialPosition[1]] = ' ';
     grid[penguPosition[0]][penguPosition[1]] = 'X';
-    // log(grid, pathArray, fishCount)
     console.log('----------')
     return [pathArray, fishCount, grid];
   }
-  console.log(penguPosition);
+  // Below block checks for 2 conditions
+  // 1. When isInitialPenguPosition is used to trigger the recursion only once and sets to false
+  // 2. When penguPosition in the grid is an Snow Cell
   if((isInitialPenguPosition || grid[penguPosition[0]][penguPosition[1]] == '0') && counter < 6){
+    // generating all possible moves for the pengu by taking pengu position
     moves = getNextMoves(penguPosition);
     isInitialPenguPosition = false;
     const [direction, randomMove] = getRandomValidMove(moves, grid);
@@ -78,14 +86,15 @@ function penguSlider(grid, penguPosition, counter){
     log('Fish got caught!', penguPosition); 
     fishCount++;
     grid[penguPosition[0]][penguPosition[1]] = ' ';
-    if(fishCount === initailFishcount){
+    // if penguin eats all the fishes, then terminate
+    if(fishCount === totalFishcount){
       return [pathArray, fishCount, grid];
     }
-    //log('Fish got caught!', penguPosition,'direction',direction,'nextMove', nextMove, randomMove);
   }
+  // penguin checks for the ice cell
   if(grid[penguPosition[0]][penguPosition[1]] == ' '){
-    //const nextMove = continueInTheSameDirection(direction, randomMove)
-    const direction = pathArray[pathArray.length-1];
+    //const nextMove = continueInTheSameDirection(direction, randomMove) 
+    const direction = pathArray[pathArray.length-1]; // recent direction of the pengu
     const nextMove = continueInTheSameDirection(direction, penguPosition)
     if(grid[nextMove[0]][nextMove[1]] == '#' && counter < 6){
       moves = getNextMoves(penguPosition);
@@ -94,10 +103,10 @@ function penguSlider(grid, penguPosition, counter){
       counter++;
       return penguSlider(grid, randomMove1, counter);
     }else if(grid[nextMove[0]][nextMove[1]] != '#'){
-      console.log('asdfasdf')
       return penguSlider(grid, nextMove, counter)
     }
   }
+  // if the counter is 6, then return
   if(counter == 6){
     console.log('----------')
     console.log('Counter 6')
@@ -108,7 +117,7 @@ function penguSlider(grid, penguPosition, counter){
   }
 }
 
-
+// Writes the output to teh file
 function writeOutputToFile(output){
   const [pathArray, fishCount, grid] = output;
   const content = `${pathArray.join('')}\n${fishCount}\n${grid.map(i => i.join('')).join('\n')}`;
@@ -118,11 +127,9 @@ function writeOutputToFile(output){
       console.error(err)
       return
     }
-    //file written successfully
-    log(`Printed the output to ${outputFile}`)
+    log(`Successfully wrote the output to ${outputFile}`)
   })
 }
-
 
 // Perform this when Pengu gets stuck by hitting a Wall or by the Snow cell
 function getNextMoves(penguPosition){
@@ -133,6 +140,7 @@ function getNextMoves(penguPosition){
   return moves;
 }
 
+// Perform this to get a Random valid move after pengu hits by a Wall or by the Snow cell
 function getRandomValidMove(moves, grid){
   const randMoves = [];
   moves.forEach((item) => {
@@ -147,7 +155,8 @@ function getRandomValidMove(moves, grid){
   return [randMoves[randomIndex][0], randMoves[randomIndex][1]]
 }
 
-function continueInTheSameDirection(direction, currentPos, multiplier){
+ // Anticipating the next move based on direction and current pengu position
+function continueInTheSameDirection(direction, currentPos){
   const coordinateForTheDirection = directions.find(dir => dir[0] == direction)[1];
   const [nextX, nextY] = coordinateForTheDirection;
   const nextMove = [currentPos[0]+nextX, currentPos[1]+nextY]
